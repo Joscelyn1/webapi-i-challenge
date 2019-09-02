96da867439910d35505bfa35453c376550f434b0
Joscelyn <JoscelynRose@gmail.com>
2019-09-02 12:06:01 -0400

    set up server to /

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // implement your API here
+const express = require('express');
+const Data = require('./data/db.js');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.send('hello Joscelyn');
+});
+
+const port = 5000;
+
+server.listen(port, () => console.log('api running'));
